cfc37cdbee50419467db99fda1a95956c81d2bf2
Pensiveant <1429055703@qq.com>
2021-02-01 15:46:17 +0800

    个人网站首页添加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from './App.less';
 
+let contentList = [{
+  title: '博客（blog）',
+  url: './blog'
+}, {
+  title: 'ArcGIS开发',
+  url: './arcgis'
+}]
 function App() {
+
   return (
     <div className={styles.app}>
-      个人网站入口
+      <div className={styles.contentPannel}>
+        <div className={styles.title}>
+          <p> Pensiveant的个人网站</p>
+        </div>
+        <div className={styles.contentList}>
+          {contentList.map((item) => {
+            return (
+              <div className={styles.item}>
+                <a href={item.url}>{item.title}</a>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
